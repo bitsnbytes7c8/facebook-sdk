518ccdd2b7ad52ceceaba2eb6e59191dcbcd3e07
Arjun S Nath <arjun.sn7c8@gmail.com>
2014-08-08 19:31:46 +0530

    Fixed error in file write statement

--- get_message_senders.py
+++ get_message_senders.py
@@ -9,21 +9,22 @@ def get_senders(filename):
     print "Querying for " + str(i) + " " + str(i+49);
     try:
       senders_dict = message_senders.get_fifty(i, i+49);
+      i += 50;
+      print "Received dictionary.\nProcessing....";
+      if len(senders_dict.keys()) == 0:
+        break;
+      for key in senders_dict.keys():
+        if key not in senders.keys():
+          senders[key] = senders_dict[key];
+      print "Finished processing dictionary";
+      break;
     except:
-      print "Failed\n";
+      print "Failed to process for " + str(i) + " " + str(i+49);
       i+=50;
       continue;
-    i += 50;
-    print "Received dictionary.\nProcessing....";
-    if len(senders_dict.keys()) == 0:
-      break;
-    for key in senders_dict.keys():
-      if key not in senders.keys():
-        senders[key] = senders_dict[key];
-    print "Finished processing dictionary";
   for key in senders.keys():
     try:
-      f.write(senders[key] + "\t" + key + "\n");
+      f.write(str(senders[key]) + "\t" + str(key) + "\n");
     except(TypeError):
       sys.stderr.write("User not found - probably a group mesg\n");
   return;
